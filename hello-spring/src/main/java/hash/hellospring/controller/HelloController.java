ca2feb2c234ef057e833a7a30a5b39d1da87eb4e
byungchul_yun <byung-chul@naver.com>
2022-09-01 22:27:38 +0900

    Web 개발 기초 중 3가지에 대해 공부함 - Static contents 	- uri상으로 원하는 file을 붙여서 사용 	- spring project 내부 static directory에 해당하는 file 이 있으면 	  해당 file rendering - MVC Pattern을 이용한 rendering template engine 	- 특정 uri로 request 요청 시 controller에서 해당 request를 받음 	  (Controller) 	- Controller에서 Model에 정보를 담음 (key-value로 구성되는 	  model) 	- String으로 template의 이름을 return 시 viewResolver에서 	  해당하는 template을 찾아서 해당 html을 return함 (view) 		- template 내부에서 model의 정보를 이용할 수 있음 - API 	- Request에 대한 처리를 return 		- return은 대부분 json 형식으로 함 (xml형식도 가능) 	- return 되는 object나 값과 request의 요청 type을 종합하여 	  httpResponseConverter를 골라서 변환하여 return

--- hello-spring/src/main/java/hash/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hash/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hash.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,5 +15,37 @@ public class HelloController {
         // 문자로 return 시 viewResolver 가 hello 를 기준으로 resources/templates 에서 찾아서 hello.html 을 rendering
         // viewResolver 는 resources:templates/ + {ViewName} + .html 을 기본으로 mapping
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
