4cc98e4255a6b15f2ca999587e4732c84f0497be
byungchul_yun <byung-chul@naver.com>
2022-09-20 20:56:49 +0900

    java code를 이용한 Bean 설정 Bean으로 설정되는 class들을 원하는 때에 코드를 건들지 않고 쉽게 변경하기 용이한 방법임 본 강의에서의 예시는 Database에 관련된것이 정해지지 않은 상태에서 일단 memory DB방식으로 구현을 하고, 추후에 Database관련되는 부분만 갈아끼울 수 있도록 하기 위함

---
+++ hello-spring/src/main/java/hash/hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package hash.hellospring;
+
+import hash.hellospring.repository.MemberRepository;
+import hash.hellospring.repository.MemoryMemberRepository;
+import hash.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
